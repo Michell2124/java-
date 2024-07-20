62a727c5428cbafc3cd0537cbce2914c70d55cc1
Michell2124 <mimmichell34@gmail.com>
2024-07-20 00:57:47 +0000

    suma de numeros

---
+++ ejercicios/suma.java
@@ -0,0 +1,9 @@
+public class suma{
+    public static void main(String[] args){
+        int numero = 12;
+        int numero2 = 2;
+        int suma = numero + numero2;
+        System.out.println(suma);
+        
+    }
+}
